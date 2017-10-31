bfba4109fb557913e542873ca0a61290973447a5
zhenyiyi <15618211029@163.com>
2017-10-31 18:07:16 +0800

    boost signal2 signal

--- learnBoost/learnBoost/function.cpp
+++ learnBoost/learnBoost/function.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by fenglin on 2017/10/30.
 //  Copyright © 2017年 fenglin. All rights reserved.
-//
+//  http://www.cnblogs.com/sld666666/archive/2010/12/16/1907591.html
 
 #include <stdio.h>
 #include <iostream>
@@ -27,10 +27,33 @@ void print(int index, int total,const string &info){
     << info << endl;
 }
 
+class X {
+public:
+    int foo(int a){
+        cout << "foo a==" << a << endl;
+        return a;
+    }
+    
+public:
+    
+};
+
+
 int main(int argc, const char * argv[]) {
     
     test(100,print);
     test(100,boost::bind(&print ,_1,_2,"compute->"));
+    
+    boost::function<int (X*,int) > f;
+    
+    f = &X::foo;
+    
+    X x1;
+    
+    f(&x1,5);
+    
+
+    
     return 0;
 }
 
---
+++ learnBoost/learnBoost/signal2.cpp
@@ -0,0 +1,30 @@
+//
+//  signal2.cpp
+//  learnBoost
+//
+//  Created by fenglin on 2017/10/31.
+//  Copyright © 2017年 fenglin. All rights reserved.
+//
+
+#include <stdio.h>
+#include "boost/signals2.hpp"
+
+static void app_event(bool isActice){
+    printf("isActice == %d\n",isActice);
+}
+
+static boost::signals2::signal<void (bool isActice)> sig;
+
+static int boot_run(){
+    sig.connect(&app_event);
+    return 0;
+}
+
+static int app_logic = boot_run();
+
+int main(){
+    sig(false);
+    sleep(3);
+    sig(true);
+    return 0;
+}
